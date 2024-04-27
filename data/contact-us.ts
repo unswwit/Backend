d0d29e00890e721b4b7f088747cfd66a8ca2f08e
Merlte <wstly92@gmail.com>
2024-04-27 18:26:19 +1000

    refactor: contact us profiles into new data file

---
+++ data/contact-us.ts
@@ -0,0 +1,53 @@
+// data for WIT committee profiles
+const profiles = [
+    {
+        name: "Brianna Kim",
+        src: "/icons/pres-icon.png",
+        alt: "pres-icon",
+        mail: "president@unswwit.com",
+        mailto: "mailto:president@unswwit.com",
+    }, {
+        name: "Vivian Wang",
+        src: "/icons/pres-icon.png",
+        alt: "pres-icon",
+        mail: "president@unswwit.com",
+        mailto: "mailto:president@unswwit.com",
+    }, {
+        name: "Yuhan Zhou",
+        src: "/icons/sec-icon.png",
+        alt: "sec-icon",
+        mail: "secretary@unswwit.com",
+        mailto: "mailto:secretary@unswwit.com",
+    }, {
+        name: "Abigail Joesan Johan",
+        src: "/icons/tr-icon.png",
+        alt: "tr-icon",
+        mail: "treasurer@unswwit.com",
+        mailto: "mailto:treasurer@unswwit.com",
+    }, {
+        name: "Emily Dang",
+        src: "/icons/ex-icon.png",
+        alt: "ex-icon",
+        mail: "externals@unswwit.com",
+        mailto: "mailto:externals@unswwit.com",
+    }, {
+        name: "Abarna Ravindran",
+        src: "/icons/int-icon.png",
+        alt: "int-icon",
+        mail: "internals@unswwit.com",
+        mailto: "mailto:internals@unswwit.com",
+    }, {
+        name: "Joanna He",
+        src: "/icons/tech.png",
+        alt: "it-icon",
+        mail: "technology@unswwit.com",
+        mailto: "mailto:technology@unswwit.com",
+    }, {
+        name: "Chelsea Chaffey",
+        src: "/icons/multi-icon.png",
+        alt: "multi-icon",
+        mail: "multimedia@unswwit.com",
+        mailto: "mailto:multimedia@unswwit.com",
+    }]
+
+export default profiles;
--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -7,6 +7,7 @@ import LoadingScreen from '../components/LoadingScreen';
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
+import profiles from '../data/contact-us'
 
 const ContactUs = () => {
   const [sourceLoading, setSourceLoading] = useState(true);
@@ -41,223 +42,33 @@ const ContactUs = () => {
 
           <div className={styles.sections}>
             <div className={styles.team}>
-              <h2 className={styles.contactTeam}>Looking for Someone Specific?</h2>
+              <h2 className={styles.contactTeam}>
+                Looking for Someone Specific?
+              </h2>
               <div className={styles.grid}>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/pres-icon.png"
-                    alt="pres-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Brianna Kim</h3>
-                  <Link href="mailto:president@unswwit.com">
-                    president@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/pres-icon.png"
-                    alt="pres-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Vivian Wang</h3>
-                  <Link href="mailto:president@unswwit.com">
-                    president@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/sec-icon.png"
-                    alt="sec-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Yuhan Zhou</h3>
-                  <Link href="mailto:secretary@unswwit.com">
-                    secretary@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/tr-icon.png"
-                    alt="tr-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Abigail Joesan Johan</h3>
-                  <Link href="mailto:treasurer@unswwit.com">
-                    treasurer@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/ex-icon.png"
-                    alt="ex-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Emily Dang</h3>
-                  <Link href="mailto:externals@unswwit.com">
-                    externals@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/int-icon.png"
-                    alt="int-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Abarna Ravindran</h3>
-                  <Link href="mailto:internals@unswwit.com">
-                    internals@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/tech.png"
-                    alt="it-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Joanna He</h3>
-                  <Link href="mailto:technology@unswwit.com">
-                    technology@unswwit.com
-                  </Link>
-                </div>
-                <div className={styles.profile}>
-                  <Image
-                    src="/icons/multi-icon.png"
-                    alt="multi-icon"
-                    width="50"
-                    height="50"
-                  />
-                  <h3>Chelsea Chaffey</h3>
-                  <Link href="mailto:multimedia@unswwit.com">
-                    multimedia@unswwit.com
-                  </Link>
-                </div>
+                {profiles.map((profile) => (
+                  <div className={styles.profile}>
+                    <Image
+                      src={profile.src}
+                      alt={profile.alt}
+                      width="50"
+                      height="50"
+                    />
+                    <h3>{profile.name}</h3>
+                    <Link
+                      href={profile.mailto}
+                    >{profile.mail}</Link>
+                  </div>
+                ))}
               </div>
             </div>
 
             <div className={styles.form}>
               <ContactForm />
             </div>
-
           </div>
-
-          {/* <div className={styles.newEmails}>
-            <div className={styles.col2}>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/pres-icon.png"
-                  alt="pres-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Brianna Kim</h3>
-                <Link href="mailto:president@unswwit.com">
-                  president@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/pres-icon.png"
-                  alt="pres-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Vivian Wang</h3>
-                <Link href="mailto:president@unswwit.com">
-                  president@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/sec-icon.png"
-                  alt="sec-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Yuhan Zhou</h3>
-                <Link href="mailto:secretary@unswwit.com">
-                  secretary@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/tr-icon.png"
-                  alt="tr-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Abigail Joesan Johan</h3>
-                <Link href="mailto:treasurer@unswwit.com">
-                  treasurer@unswwit.com
-                </Link>
-              </div>
-            </div>
-
-            <hr
-              className={[styles.lineBreak, styles.solid].join(' ')}
-              width="90%"
-            />
-            <div className={styles.col3}>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/ex-icon.png"
-                  alt="ex-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Emily Dang</h3>
-                <Link href="mailto:externals@unswwit.com">
-                  externals@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/int-icon.png"
-                  alt="int-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Abarna Ravindran</h3>
-                <Link href="mailto:internals@unswwit.com">
-                  internals@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/tech.png"
-                  alt="it-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Joanna He</h3>
-                <Link href="mailto:technology@unswwit.com">
-                  technology@unswwit.com
-                </Link>
-              </div>
-              <div className={styles.profile}>
-                <Image
-                  src="/icons/multi-icon.png"
-                  alt="multi-icon"
-                  width="50"
-                  height="50"
-                />
-                <h3>Chelsea Chaffey</h3>
-                <Link href="mailto:multimedia@unswwit.com">
-                  multimedia@unswwit.com
-                </Link>
-              </div>
-            </div>
-          </div> */}
         </div>
-      )
-      }
+      )}
     </div >
   );
 };
