0a02a9065093ee6af43e2e6a55d3cdc83ba0ce03
Yukiboki <158557395+Yukiboki@users.noreply.github.com>
2024-10-05 00:57:54 +1000

    WIT-217 updating the new executives for 2024 (#99)
    * updated the 2024 executives
    
    * fixed up lint error
    
    ---------
    
    Co-authored-by: Sarah Ho <z5481589@ad.unsw.edu.au>
    Co-authored-by: Silvia <37344470+ItsLame@users.noreply.github.com>

--- data/contact-us.ts
+++ data/contact-us.ts
@@ -1,49 +1,49 @@
 // data for WIT committee profiles
 const profiles = [
     {
-        name: 'Brianna Kim',
+        name: 'Angela Joyce Chacko',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Vivian Wang',
+        name: 'Charlotte Fang Hendro',
         src: '/icons/pres-icon.png',
         alt: 'pres-icon',
         mail: 'president@unswwit.com',
         mailto: 'mailto:president@unswwit.com',
     }, {
-        name: 'Yuhan Zhou',
+        name: 'Amy Tian',
         src: '/icons/sec-icon.png',
         alt: 'sec-icon',
         mail: 'secretary@unswwit.com',
         mailto: 'mailto:secretary@unswwit.com',
     }, {
-        name: 'Abigail Joesan Johan',
+        name: 'Alice Shang',
         src: '/icons/tr-icon.png',
         alt: 'tr-icon',
         mail: 'treasurer@unswwit.com',
         mailto: 'mailto:treasurer@unswwit.com',
     }, {
-        name: 'Emily Dang',
+        name: 'Crystal Zhao',
         src: '/icons/ex-icon.png',
         alt: 'ex-icon',
         mail: 'externals@unswwit.com',
         mailto: 'mailto:externals@unswwit.com',
     }, {
-        name: 'Abarna Ravindran',
+        name: 'Elissa Luong',
         src: '/icons/int-icon.png',
         alt: 'int-icon',
         mail: 'internals@unswwit.com',
         mailto: 'mailto:internals@unswwit.com',
     }, {
-        name: 'Joanna He',
+        name: 'Grace Kan',
         src: '/icons/tech.png',
         alt: 'it-icon',
         mail: 'technology@unswwit.com',
         mailto: 'mailto:technology@unswwit.com',
     }, {
-        name: 'Chelsea Chaffey',
+        name: 'Neysa Njo',
         src: '/icons/multi-icon.png',
         alt: 'multi-icon',
         mail: 'multimedia@unswwit.com',
