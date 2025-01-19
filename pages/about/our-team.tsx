43d44860534dca413d14ceb58a0bfba13cc1d2ce
valeriechan36 <159455273+valeriechan36@users.noreply.github.com>
2025-01-20 03:18:12 +1100

    Update our-team.tsx

--- pages/about/our-team.tsx
+++ pages/about/our-team.tsx
@@ -198,7 +198,7 @@ const OurTeam = ({ execs, subcommittee, directors }: any) => {
           <Timeline
             margin={'50px'}
             page={'teams'}
-            step={14.2}
+            step={12.5}
             valueToYear={valueToYear}
             marks={marks}
             updateYear={handleYear}
